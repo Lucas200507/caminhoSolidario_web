fe2d3e82ff6ef5a8e7bf5a2caa851817c97e3a7c
Lucas Ataide <lucasataide.martins@a.ucb.br>
2025-04-30 11:25:50 -0300

    Finalização e inicio de telas
    Telas Finalizadas: cadastro_voluntario e cadastro_beneficiario.
    Tela iniciada: cadastro_beneficiente

--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
-function alterar_cadastrarBeneficiario(){
+// NAVEGAÇÃO DE PÁGINAS
+function navegar_cadastrarBeneficiario(){
     window.location.href = "/html/cadastro/cadastroBeneficiario.html";
     
+}
+
+function navegar_cadastrarVoluntario(){
+    window.location.href = "/html/cadastro/cadastroVoluntario.html";
+    
+}
+
+function navegar_cadastrarDependente(){
+    window.location.href = "/html/cadastro/cadastroDependente.html";
+    
+}
+
+
+function cancelarCadastro(){
+    window.alert("Cancelou meu nobre");
+}
+
+function Cadastrar_Voluntario(){
+    senhaTemporaria = document.getElementById('senhaTemporaria').value;
+    confirmarSenha = document.getElementById('confirmarSenha').value;
+    if (senhaTemporaria == confirmarSenha){
+        window.alert('As senhas são enguais');
+    } else {
+        window.alert('As senhas são deferentes');
+    }
 }
