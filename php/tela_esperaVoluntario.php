a1755e8428890ee3c023fe1730292d64d38a36d1
Lucas Ataide <163359192+Lucas200507@users.noreply.github.com>
2025-07-01 11:25:47 -0300

    Update tela_esperaVoluntario.php

--- php/tela_esperaVoluntario.php
+++ php/tela_esperaVoluntario.php
@@ -1,3 +1,25 @@
+<?php   
+    $campo_vazio = False;
+    $cadastrado = False;
+     if(isset($_POST['submit']) && !empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['telefone']) && !empty($_POST['cpf']) && !empty($_POST['estado'])){
+         // Conectando com o banco: 
+         include_once('../conexao_banco.php'); // ACESSANDO A CONEXÃO
+         
+         $nome = $_POST['nome'];
+         $email = $_POST['email'];
+         $telefone = $_POST['telefone'];
+         $cpf = $_POST['cpf'];
+         $estado = $_POST['estado'];
+        
+        $sql = "INSERT INTO espera_voluntario(nome_completo, email, telefone, cpf, estado) VALUES('$nome','$email','$telefone','$cpf','$estado')";
+        $cadastrado = True;        
+        
+     }  else if(isset($POST['submit']) && (empty($POST['nome']) || empty($POST['email']) || empty($POST['telefone']) || empty($POST['cpf']) || empty($POST['estado']))){
+        $campo_vazio = True;
+     }
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,7 +29,7 @@
     <title>Solicitar criaçao de conta</title>
     <link rel="stylesheet" href="../css/index.css">
     <!-- BOOTSTRAP CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     
     <!-- Conexao com JS -->
     <script src="../js/index.js" defer></script> 
@@ -18,17 +40,29 @@
        <p class="h3 text-center p-1">Coloque seus dados para entrarmos em contato</p>
         <form class="d-block p-0 form_login" method="post" action="">
             <div class="form-group d-flex flex-column mt-3">
-                <label for=""><input type="text" placeholder="Nome:" class="mb-2 form-control border-primary text-dark w-100" name="nome"></label>
-                <label for=""><input type="text" placeholder="Email:" class="mb-2 form-control border-primary text-dark w-100" name="email"></label>
-                <label for=""><input type="text" placeholder="Telefone:" class="mb-2 form-control border-primary text-dark w-100" name="telefone"></label>
-                <label for=""><input type="text" placeholder="CPF:" class="mb-2 form-control border-primary text-dark w-100" name="cpf"></label>
-                <select name="estado"  class="form-select form-select-md w-50">
+                <label for=""><input type="text" placeholder="Nome:" class="mb-2 form-control border-primary text-dark w-100" name="nome" required></label>
+                <label for=""><input type="email" placeholder="Email:" class="mb-2 form-control border-primary text-dark w-100" name="email" required></label>
+                <label for=""><input type="tel" placeholder="Telefone:" class="mb-2 form-control border-primary text-dark w-100" name="telefone" required></label>
+                <label for=""><input type="text" placeholder="CPF:" class="mb-2 form-control border-primary text-dark w-100" name="cpf" required></label>
+                <select name="estado"  class="form-select form-select-md w-100 mb-2 border-primary" required>
                     <option value="">Estado: </option>
                     <option value="DF">Distrito Federal</option>
                     <option value="outros">Outros</option>
                 </select>              
             </div>            
-            <input type="submit" name="submit" value="Enviar" class="btn btn-light btn-outline-primary btn-block btn-lg mb-2">        
+            <input type="submit" name="submit" value="Enviar" class="d-block w-100 btn btn-light btn-outline-primary btn-block btn-lg mt-2 mb-2">   
+            <!-- BACK END PARA CADASTRAR NA LISTA DE ESPERA -->
+            <?php if($cadastrado): ?>               
+                <script>         
+                    window.alert('Você está na lista de espera. Por favor, aguarde o recebimento do email.');                                                   
+                    window.location.href = "login.php";
+                    
+                </script>                
+            <?php elseif($campo_vazio): ?>
+                <div id="mensagem_campoVazio_esperaVoluntario" class="container_mensagem_esperaVoluntario">
+                    É necessário preencher todos os campos.
+                </div>
+             <?php endif; ?>  
             <a href="login.php">
                 <ion-icon name="arrow-back-circle-outline" id="btVoltar"></ion-icon>
             </a>
@@ -44,20 +78,3 @@
          <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
-<?php
-    // if(isset($_POST['submit']) && !empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['telefone']) && !empty($_POST['cpf']) && !empty($_POST['estado']))){
-    //     // Conectando com o banco: 
-    //     include_once('../conexao_banco.php'); // ACESSANDO A CONEXÃO
-
-    //     $nome = $_POST['nome'];
-    //     $email = $_POST['email'];
-    //     $telefone = $_POST['telefone'];
-    //     $cpf = $_POST['cpf'];
-    //     $estado = $_POST['estado'];
-        
-    //     $sql = "INSERT INTO espera_voluntario(nome, email, telefone, cpf, estado) VALUES('$nome','$email','$telefone','$cpf','$estado')";
-
-    // }
-
-
-?>
