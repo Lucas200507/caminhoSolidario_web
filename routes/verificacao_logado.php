dafd57ce84a784246a29fdc3f7799d14ec509d17
Lucas Ataide <lucasataide.martins@a.ucb.br>
2025-07-03 11:39:57 -0300

    2

--- routes/verificacao_logado.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    // Verificando se fez o login
-    if ((!isset($_SESSION['usuario']) && !isset($_SESSION['senha'])) || isset($_SESSION['deslogar'])){
-        unset($_SESSION['usuario']);
-        unset($_SESSION['senha']);
-        header('Location: ../php/login.php');
-    }
-?>
