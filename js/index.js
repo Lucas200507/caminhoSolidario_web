5d10c38cd2e51f90735f0580488429ac0e30abb8
Lucas Ataide <lucasataide.martins@a.ucb.br>
2025-07-24 20:16:58 -0300

    desabilitando - resetarSenha

--- js/index.js
+++ js/index.js
@@ -126,14 +126,14 @@ function iconHoverOut(){
 }
 
 
-function resetar_senha(){
-    let container = document.getElementById('formResetarSenha');
-    if (container.style.display == 'none'){
-        container.style.display =  'block';
-    } else {
-        container.style.display = 'none';
-        }
-}
+// function resetar_senha(){
+//     let container = document.getElementById('formResetarSenha');
+//     if (container.style.display == 'none'){
+//         container.style.display =  'block';
+//     } else {
+//         container.style.display = 'none';
+//         }
+// }
 
 function logout(){
     window.alert('Saindo!!!');
